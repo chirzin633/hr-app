c89a8b505bde3e75d541b0ba4b62b557ea1a2607
chirzin633 <chirzin.attabiq@gmail.com>
2026-09-19 10:17:35 +0700

    dashboard: create layout

---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <header class="mb-3">
+        <a href="#" class="d-block burger-btn d-xl-none">
+            <i class="bi bi-justify fs-3"></i>
+        </a>
+    </header>
+
+    <div class="page-heading">
+        <h3>Task</h3>
+    </div>
+@endsection
